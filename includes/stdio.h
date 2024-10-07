f800d50c72c0c6e0b29bae0d3bba95c67f5fd9ff
resoinaut <resoinaut@gmail.com>
2024-10-07 11:41:15 -0600

    printf fix

--- includes/stdio.h
+++ includes/stdio.h
@@ -25,10 +25,12 @@ int printf(const char *__restrict format, ...)
 	// TODO: implement formatting and varargs
 	// TODO: look into moving GetStdHandle into the global scope
 
-	HANDLE handle  = GetStdHandle(STD_OUTPUT_HANDLE);
-	BOOL   success = WriteConsoleA(handle, format, strlen(format), NULL, NULL);
+	DWORD count;
 
-	if (!success) return -1;
+	HANDLE handle  = GetStdHandle(STD_OUTPUT_HANDLE);
+	BOOL   success = WriteConsoleA(handle, format, strlen(format), &count, NULL);
+
+	return success ? count : -1;
 }
 
 #endif // header guard
